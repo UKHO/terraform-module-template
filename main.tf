eab882a660a70960e812cf4ecc49a514ee8af276
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2020-02-14 14:10:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,37 @@
-resource "azurerm_resource_group" "rg" {
-    name = "${var.name}-RG"  
-    location = "UK South"
+data "azurerm_client_config" "current" {}
+
+data "azurerm_subscription" "current" {}
+
+locals {
+  base_cidr_block = var.address
 }
 
+data "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.ProjectIdentity}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_virtual_network" "spokevnet" {
+  name                = "${var.ProjectIdentity}-vnet"
+  address_space       = [local.base_cidr_block]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+    dynamic "subnet" {
+      for_each = [for s in var.subnets : {
+        name   = s.name
+        prefix = cidrsubnet("${local.base_cidr_block}", var.newbits, s.number)
+      }]
+  
+      content {
+        name           = subnet.value.name
+        address_prefix = subnet.value.prefix
+        security_group = azurerm_network_security_group.nsg.id             
+      }
+    }
+}
